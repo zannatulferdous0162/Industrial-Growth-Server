0f03b65ca4a2acb3c5cfe0e79b7a70def9d4e265
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-05-16 22:01:39 +0600

    change successfully mongo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.jobDB}:${process.env.password}@cluster0.ugmduxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
